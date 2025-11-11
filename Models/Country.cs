a3ab559c4f6aa3f3062dcbe55a139c57d330a400
kashmije <Narutopro101@yandex.ru>
2025-11-12 02:02:55 +0300

    lol models

---
+++ Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uchebnayaPractica.Models;
+
+public partial class Country
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string NameCountry { get; set; } = null!;
+
+    public string NameEng { get; set; } = null!;
+
+    public virtual ICollection<User> Iduser { get; set; } = new List<User>();
+}
---
+++ Models/TypeofEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uchebnayaPractica.Models;
+
+public partial class TypeofEvent
+{
+    public int Id { get; set; }
+
+    public string TypeName { get; set; } = null!;
+
+    public virtual ICollection<EventTypeDictionary> EventTypeDictionary { get; set; } = new List<EventTypeDictionary>();
+}
