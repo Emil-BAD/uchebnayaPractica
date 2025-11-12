f15b7ea952b06aeb38abe35b139078b92f33c70e
iskan <isko2024mail.ru>
2025-11-12 14:02:09 +0300

    Программное на главной

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,75 +1,122 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using uchebnayaPractica.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace uchebnayaPractica
 {
     public partial class MainWindow : Window
     {
-        private List<Event> _allEvents;
+        private readonly Praktika2Context _context;
 
         public MainWindow()
         {
             InitializeComponent();
+            _context = new Praktika2Context();
+            LoadDirections();
             LoadEvents();
-            FillDirectionFilter();
-            ApplyFilter();
         }
 
-        private void LoadEvents()
+        // Загрузка направлений
+        private void LoadDirections()
         {
-            _allEvents = new List<Event>
-            {
-                new Event { Logo="Images/logo1.png", Title="Хакатон 2025", Direction="Программирование", Date=new DateTime(2025,11,20)},
-                new Event { Logo="Images/logo2.png", Title="Дизайн-марафон", Direction="Дизайн", Date=new DateTime(2025,12,01)},
-                new Event { Logo="Images/logo3.png", Title="Блокчейн-день", Direction="Криптотехнологии", Date=new DateTime(2025,11,25)},
-                new Event { Logo="Images/logo4.png", Title="Data Science Meetup", Direction="Аналитика", Date=new DateTime(2025,12,05)},
-            };
-        }
-
-        private void FillDirectionFilter()
-        {
-            var directions = _allEvents.Select(e => e.Direction).Distinct().ToList();
+            var directions = _context.Direction
+                .Select(d => d.DirectionName)
+                .ToList();
             directions.Insert(0, "Все направления");
             DirectionFilter.ItemsSource = directions;
             DirectionFilter.SelectedIndex = 0;
         }
 
+        // Загрузка всех мероприятий
+        private void LoadEvents()
+        {
+            var events = _context.Event
+                .Include(e => e.EventTypeDictionary)
+                    .ThenInclude(etd => etd.IddictionaryNavigation)
+                .ToList()
+                .Select(e => new
+                {
+                    Title = e.NameEvent,
+                    Date = e.DateStart,
+                    Days = e.Days,
+                    Direction = string.Join(", ",
+                        e.EventTypeDictionary
+                            .Select(etd => etd.IddictionaryNavigation.DirectionName)
+                            .Distinct()),
+                    // Сопоставляем логотип по ID события
+                    Logo = GetLogoPath(e.Id)
+                })
+                .ToList();
+            EventList.ItemsSource = events;
+        }
+
+        // Применение фильтров
         private void ApplyFilter()
         {
-            var filtered = _allEvents.AsEnumerable();
+            var filtered = _context.Event
+                .Include(e => e.EventTypeDictionary)
+                    .ThenInclude(etd => etd.IddictionaryNavigation)
+                .AsQueryable();
 
             // Фильтр по направлению
             if (DirectionFilter.SelectedItem != null && DirectionFilter.SelectedItem.ToString() != "Все направления")
             {
-                string dir = DirectionFilter.SelectedItem.ToString();
-                filtered = filtered.Where(e => e.Direction == dir);
+                string selectedDirection = DirectionFilter.SelectedItem.ToString();
+                filtered = filtered.Where(e => e.EventTypeDictionary
+                    .Any(etd => etd.IddictionaryNavigation.DirectionName == selectedDirection));
             }
 
             // Фильтр по дате
-            if (DateFilter.SelectedDate != null)
+            if (DateFilter.SelectedDate.HasValue)
             {
-                DateTime date = DateFilter.SelectedDate.Value;
-                filtered = filtered.Where(e => e.Date.Date == date.Date);
+                DateOnly selectedDate = DateOnly.FromDateTime(DateFilter.SelectedDate.Value);
+                filtered = filtered.Where(e => e.DateStart == selectedDate);
             }
 
-            EventList.ItemsSource = filtered.ToList();
+            var result = filtered
+                .ToList()
+                .Select(e => new
+                {
+                    Title = e.NameEvent,
+                    Date = e.DateStart,
+                    Days = e.Days,
+                    Direction = string.Join(", ",
+                        e.EventTypeDictionary
+                            .Select(etd => etd.IddictionaryNavigation.DirectionName)
+                            .Distinct()),
+                    // Сопоставляем логотип по ID события
+                    Logo = GetLogoPath(e.Id)
+                })
+                .ToList();
+            EventList.ItemsSource = result;
         }
 
-        private void FilterChanged(object sender, EventArgs e)
+        // Универсальный метод для получения пути к логотипу (сопоставление по ID события, цикл 1-20)
+        private string GetLogoPath(int eventId)
+        {
+            int logoIndex = (eventId - 1) % 20 + 1; // Получаем 1..20 на основе eventId
+            return $"pack://application:,,,/Resources/{logoIndex}.jpeg";
+        }
+
+        // Обработчики фильтров
+        private void DirectionFilter_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             ApplyFilter();
         }
-    }
-}
 
-      /*  private void AuthButton_Click(object sender, RoutedEventArgs e)
+        private void DateFilter_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-           // var loginWindow = new LoginWindow();
-            //loginWindow.Show();
+            ApplyFilter();
+        }
+
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            var authWindow = new Auth();
+            authWindow.Show();
             this.Close();
         }
     }
 }
-*/
